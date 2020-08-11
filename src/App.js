88177d3c2662cd8ebe531bb7f0f4303ec778fccc
liaowenqiang828 <18840965351@163.com>
2020-08-11 20:41:19 +0800

    [Wenqiang Liao] feat: add page container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Header from './component/Header';
+import Container from "./component/Container";
 
 class App extends Component {
   render() {
     return (
       <main className="app">
         <Header/>
+        <Container/>
       </main>
     );
   }
