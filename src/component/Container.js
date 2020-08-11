f9cff665a8e7370c009e4def09a0f52e9062fff4
liaowenqiang828 <18840965351@163.com>
2020-08-11 20:57:15 +0800

    [Wenqiang Liao] feat: add page two PhoneBrand container

--- src/component/Container.js
+++ src/component/Container.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import '../styles/container.css';
+import PhoneBrand from "./PhoneBrand";
 
 class Container extends Component {
     render() {
         return (
             <div className="container">
+                <PhoneBrand/>
+                <PhoneBrand/>
             </div>
         );
     }
---
+++ src/component/PhoneBrand.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../styles/phoneBrand.css';
+
+class PhoneBrand extends Component {
+    render() {
+        return (
+            <div className="phoneBrand">
+            </div>
+        );
+    }
+}
+
+export default PhoneBrand;
